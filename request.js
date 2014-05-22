854a169613b7d04866c81c4d5542a0f1600523d9
voltrue2 <voltrue2@gmail.com>
2014-05-22 12:27:37 +0900

    gracenode path fixed

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
-var gracenode = require('../../');
+var gracenode = require('../gracenode');
 var logger = gracenode.log.create('server-request');
 var queryDataHandler = require('./queryData');
 var headers = require('./headers');
