0c8d2a9e3f64ddbc8e6f76e87a6df00c89a3126c
Brian de Heus <bh@dev.2connect.jp>
2014-10-15 19:59:21 +0900

    POODLE fix

--- https.js
+++ https.js
@@ -63,7 +63,7 @@ function Https(requestHandler) {
 
 			if (constants[config.secureOptions]) {
 
-				options.secureOptions: constants.SSL_OP_NO_SSLv3;
+				options.secureOptions: constants[config.secureOptions];
 
 			} else {
 
